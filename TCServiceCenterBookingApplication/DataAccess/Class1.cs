9dcd85ac3574659e2673123a7319e8fe1a024345
Fabrice Kwitonda <fabrice.kwitonda@canada.ca>
2018-06-06 15:57:21 -0400

    Load file

--- TCServiceCenterBookingApplication/DataAccess/Class1.cs
+++ TCServiceCenterBookingApplication/DataAccess/Class1.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Xml;
 
 namespace DataAccess
 {
     public class Class1
     {
+        public void Load()
+        {
+            var xml = JsonConvert.DeserializeXmlNode(File.ReadAllText("C:/Users/fkwitond/Documents/Traca/TCServiceCenterBookingApplication/DataBase/centers.json"), "RootObject");
+            var ds = new DataSet("Json Data");
+            var xr = new XmlNodeReader(xml);
+            ds.ReadXml(xr);
+
+            var centerTypes = ds.Tables["CenterTypes"];
+
+            foreach (DataRow centerTypesRow in centerTypes.Rows)
+            {
+                Console.WriteLine(centerTypesRow["Value"]);
+
+                var insertSQL = new SQLiteCommand("INSERT INTO CenterType (Id, Value) VALUES (?,?)", sql_con);
+                insertSQL.Parameters.Add(centerTypesRow["Id"]);
+                insertSQL.Parameters.Add(centerTypesRow["Value"]);
+                try
+                {
+                    insertSQL.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+
+            }
+        }
+
     }
 }
---
+++ TCServiceCenterBookingApplication/DataAccessTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DataAccess;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace DataAccessTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var sut = new Class1();
+            sut.Load();
+        }
+    }
+}
