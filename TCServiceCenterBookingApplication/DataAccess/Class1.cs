3b73429be9b3cfda992a822fe58506173c7ea07e
Fabrice Kwitonda <Fabrice.Kwitonda@usherbrooke.ca>
2018-06-07 22:35:34 -0400

    Insert stastement

--- TCServiceCenterBookingApplication/DataAccess/Class1.cs
+++ TCServiceCenterBookingApplication/DataAccess/Class1.cs
@@ -15,22 +15,28 @@ namespace DataAccess
     {
         public void Load()
         {
-            var xml = JsonConvert.DeserializeXmlNode(File.ReadAllText("C:/Users/fkwitond/Documents/Traca/TCServiceCenterBookingApplication/DataBase/centers.json"), "RootObject");
+            //var xml = JsonConvert.DeserializeXmlNode(File.ReadAllText("C:/Users/fkwitond/Documents/Traca/TCServiceCenterBookingApplication/DataBase/centers.json"), "RootObject");
+            var xml = JsonConvert.DeserializeXmlNode(File.ReadAllText("centers.json"), "RootObject");
             var ds = new DataSet("Json Data");
             var xr = new XmlNodeReader(xml);
             ds.ReadXml(xr);
 
             var centerTypes = ds.Tables["CenterTypes"];
+            string CommandText = string.Empty;
+            foreach (DataRow centerTypesRow in centerTypes.Rows)
+            {
+                int id;
+                if (int.TryParse(centerTypesRow["Id"].ToString(), out id))
+                    CommandText += InsertStatement(id, centerTypesRow["Value"].ToString());
+            }
 
             using (var con = new SQLiteConnection("Data Source=TCDatabase.db;Version=3;"))
             {
                 con.Open();
 
                 var cmd = con.CreateCommand();
-                cmd.CommandText = string.Format("SELECT * FROM {0}", centerTypes.TableName);
-                var adapter = new SQLiteDataAdapter(cmd);
-                SQLiteCommandBuilder builder = new SQLiteCommandBuilder(adapter);
-                adapter.Update(centerTypes);
+                cmd.CommandText = CommandText;
+                cmd.ExecuteNonQuery();
                 con.Close();
             }
 
@@ -54,6 +60,17 @@ namespace DataAccess
                 createTable.ExecuteReader();
             }
         }
+
+        private string InsertStatement(int id, string value)
+        {
+            return $"INSERT INTO CenterType (Id, Value) VALUES ({id}, {value}); ";
+        }
+
+        private string InsertStatement2(int id, string value)
+        {
+            return $"INSERT INTO Center (Id, Value) VALUES ({id}, {value}); ";
+        }
+
 
     }
 }
